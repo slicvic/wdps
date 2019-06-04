d05bd38fb32a94a233de62cd9735a78440be980e
Victor Lantigua <victor@etags.com>
2019-06-04 18:12:27 -0400

    seo

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $site['meta_desc'] = !empty($phrases) ? '' : $site['app_desc'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= $site['meta_desc'] ?>">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $site['title'] ?></title>
+    <title><?= htmlspecialchars($site['title']) ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
